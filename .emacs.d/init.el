da247d5557d7f98718cbf469afd1597543d69de8
Tom Bosmans <tom.94.bosmans@gmail.com>
2020-05-02 19:55:43 +0200

    use org file for config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,2 @@
-;; Prevent 'Saving Customizations' to be written in this file
-;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Customizations.html
-(setq custom-file "~/.emacs.d/custom.el")
-(when (file-exists-p "custom.el") (load "custom"))
-
-;; custom stuff
-(add-to-list 'load-path "~/.emacs.d/customizations")
-(load "packages")
-(load "defaults")
-(load "modes")
-(load "keybindings")
+(require 'ob-tangle)
+(org-babel-load-file "~/.emacs.d/readme.org")
